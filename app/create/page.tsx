b42996d375f87427aea57c1697b14e8fe39c6202
Suhas A <suhasamaresh@gmail.com>
2024-03-23 14:17:08 +0530

    Update code formatting and fix minor bugs

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,15 +1,159 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import Sidemenu from "@/components/sidemenu";
+import { ethers } from "ethers";
+import CampaignContract from "@/contracts/CampaignContract.json";
 
-const create = () => {
+const abi = CampaignContract;
+
+declare global {
+  interface Window {
+    ethereum: any;
+  }
+}
+
+const Create = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    title: "",
+    description: "",
+    goalAmount: "",
+    deadline: "",
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
+
+    try {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+
+      const campaignContract = new ethers.Contract(
+        "0x53496afb9301d6e619d13a35798b5a32e4877a64",
+        abi,
+        signer
+      );
+
+      const deadline = new Date(formData.deadline);
+      const year = deadline.getFullYear();
+      const month = deadline.getMonth() + 1; // Month index starts from 0
+      const day = deadline.getDate();
+
+      const deadlineTimestamp = Math.floor(deadline.getTime() / 1000);
+
+      const address = await signer.getAddress();
+
+      const receipt = await campaignContract.createCampaign(
+        address,
+        formData.title,
+        formData.description,
+        parseInt(formData.goalAmount),
+        year,
+        month,
+        day,
+        "" // No image provided in this form
+      );
+
+      console.log("Campaign created successfully:", receipt);
+
+      setFormData({
+        name: "",
+        title: "",
+        description: "",
+        goalAmount: "",
+        deadline: "",
+      });
+    } catch (error) {
+      console.error("Failed to create campaign:", error);
+    }
+  };
+
   return (
-    <div className="bg-black h-screen px-24 ">
-      <div className="container bg-[#1c1c24] h-[550px]  rounded">
-        <div>
-          <h1>Create Campaign</h1>
+    <div>
+      <Sidemenu />
+      <div className="bg-black min-h-screen px-4 sm:px-6 lg:px-8 flex justify-center items-center pb-2 pt-10">
+        <div className="max-w-3xl w-full bg-[#1c1c24] px-4 py-8 sm:px-6 lg:px-8 rounded-xl">
+          <div className="bg-[#3a3a43] rounded-xl p-4">
+            <h1 className="text-white text-2xl font-semibold text-center">
+              Start a Campaign
+            </h1>
+          </div>
+          <form onSubmit={handleSubmit}>
+            <div className="md:flex justify-between">
+              <div className="mt-4">
+                <h3 className="text-[#808191] font-epilogue">Name*</h3>
+                <input
+                  type="text"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  placeholder="Your Name"
+                  className="input-field bg-transparent placeholder:text-[#808191] border-[#808191] border-2 rounded-xl p-2 w-full md:w-72 focus:outline-none focus:border-emerald-500 text-white"
+                />
+              </div>
+              <div className="mt-4">
+                <h3 className="text-[#808191]">Title*</h3>
+                <input
+                  type="text"
+                  name="title"
+                  value={formData.title}
+                  onChange={handleChange}
+                  placeholder="Campaign Title"
+                  className="input-field bg-transparent placeholder:text-[#808191] border-[#808191] border-2 rounded-xl p-2 w-full md:w-72 focus:outline-none focus:border-emerald-500 text-white"
+                />
+              </div>
+            </div>
+            <div className="mt-4">
+              <h3 className="text-[#808191]">Description*</h3>
+              <textarea
+                name="description"
+                value={formData.description}
+                onChange={handleChange}
+                placeholder="Campaign Description"
+                rows="4"
+                className="input-field bg-transparent placeholder:text-[#808191] border-[#808191] border-2 rounded-xl p-2 w-full focus:outline-none focus:border-emerald-500 text-white"
+              ></textarea>
+            </div>
+            <div className="mt-4">
+              <h3 className="text-[#808191]">Goal Amount*</h3>
+              <input
+                type="number"
+                name="goalAmount"
+                value={formData.goalAmount}
+                onChange={handleChange}
+                placeholder="Goal Amount"
+                className="input-field bg-transparent placeholder:text-[#808191] border-[#808191] border-2 rounded-xl p-2 w-full focus:outline-none focus:border-emerald-500 text-white"
+              />
+            </div>
+            <div className="mt-4">
+              <h3 className="text-[#808191]">Deadline*</h3>
+              <input
+                type="date"
+                name="deadline"
+                value={formData.deadline}
+                onChange={handleChange}
+                placeholder="Deadline"
+                className="input-field bg-transparent placeholder:text-[#808191] border-[#808191] border-2 rounded-xl p-2 w-full focus:outline-none focus:border-emerald-500 text-white"
+              />
+            </div>
+            <div className="mt-4">
+              <button
+                type="submit"
+                className="bg-emerald-500 text-white py-2 px-4 rounded-xl"
+              >
+                Submit Campaign
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
   );
 };
 
-export default create;
+export default Create;
---
+++ components/sidemenu.tsx
@@ -0,0 +1,92 @@
+import React from "react";
+import { AiOutlineHome, AiOutlineNotification, AiOutlineWallet, AiOutlineUser, AiOutlineLogout } from 'react-icons/ai';
+
+const Sidemenu = () => {
+  return (
+    <div className="hidden md:visible fixed h-screen w-16 md:flex flex-col justify-between border-r border-[#3a3a43] bg-black">
+      <div>
+        <div className="inline-flex size-16 items-center justify-center">
+          <span className="grid size-10 place-content-center rounded-lg   text-xs text-gray-400">
+            L
+          </span>
+        </div>
+
+        <div className="border-t border-gray-800">
+          <div className="px-2">
+            <div className="py-4">
+              <a
+                href="#"
+                className="group relative flex justify-center rounded   px-2 py-1.5 text-gray-400"
+              >
+                <AiOutlineHome className="size-5 opacity-75 text-gray-400" />
+
+                <span className="invisible absolute start-full top-1/2 ms-4 -translate-y-1/2 rounded bg-gray-900 px-2 py-1.5 text-xs font-medium text-white group-hover:visible">
+                  Home
+                </span>
+              </a>
+            </div>
+
+            {/* Add other menu items */}
+            <div className="py-4">
+              <a
+                href="#"
+                className="group relative flex justify-center rounded  px-2 py-1.5 text-gray-400"
+              >
+                <AiOutlineNotification className="size-5 opacity-75 text-gray-400" />
+
+                <span className="invisible absolute start-full top-1/2 ms-4 -translate-y-1/2 rounded bg-gray-900 px-2 py-1.5 text-xs font-medium text-white group-hover:visible">
+                  Announcements
+                </span>
+              </a>
+            </div>
+
+            {/* Wallet */}
+            <div className="py-4">
+              <a
+                href="#"
+                className="group relative flex justify-center rounded   px-2 py-1.5 text-gray-400"
+              >
+                <AiOutlineWallet className="size-5 opacity-75 text-gray-400" />
+
+                <span className="invisible absolute start-full top-1/2 ms-4 -translate-y-1/2 rounded bg-gray-900 px-2 py-1.5 text-xs font-medium text-white group-hover:visible">
+                  Wallet
+                </span>
+              </a>
+            </div>
+
+            {/* Profile */}
+            <div className="py-4">
+              <a
+                href="#"
+                className="group relative flex justify-center rounded   px-2 py-1.5 text-gray-400"
+              >
+                <AiOutlineUser className="size-5 opacity-75 text-gray-400" />
+
+                <span className="invisible absolute start-full top-1/2 ms-4 -translate-y-1/2 rounded bg-gray-900 px-2 py-1.5 text-xs font-medium text-white group-hover:visible">
+                  Profile
+                </span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="sticky inset-x-0 bottom-0  p-2">
+        <form action="#">
+          <button
+            type="submit"
+            className="group relative flex w-full justify-center rounded-lg  text-sm text-gray-400   hover:text-gray-300"
+          >
+            <AiOutlineLogout className="size-5 opacity-75 text-gray-400" />
+
+            <span className="invisible absolute start-full  ms-4 -translate-y-1/2 rounded text-xs font-medium text-white group-hover:visible">
+              Logout
+            </span>
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Sidemenu;
